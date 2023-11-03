e7c5192c349955ecb1854092d84018233ffb8c20
Rohit Kokate <105292137+Rohit2kokate@users.noreply.github.com>
2023-11-03 23:44:39 +0530

    footer page added

---
+++ includes/footer.php
@@ -0,0 +1,107 @@
+<footer class="page-footer">
+
+	<div class="footer-nav">
+		<div class="container clearfix">
+
+			<div class="footer-nav__col footer-nav__col--info">
+				<div class="footer-nav__heading">Information</div>
+				<ul class="footer-nav__list">
+					<li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">The brand</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">Local stores</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">Customer service</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">Privacy &amp; cookies</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">Site map</a>
+					</li>
+				</ul>
+			</div>
+
+			<div class="footer-nav__col footer-nav__col--whybuy">
+				<div class="footer-nav__heading">Why buy from us</div>
+				<ul class="footer-nav__list">
+					<li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">Shipping &amp; returns</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">Secure shipping</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">Testimonials</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">Award winning</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">Ethical trading</a>
+					</li>
+				</ul>
+			</div>
+
+			<div class="footer-nav__col footer-nav__col--account">
+				<div class="footer-nav__heading">Your account</div>
+				<ul class="footer-nav__list">
+					<li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">Sign in</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">Register</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">View cart</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">View your lookbook</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">Track an order</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">Update information</a>
+					</li>
+				</ul>
+			</div>
+
+
+			<div class="footer-nav__col footer-nav__col--contacts">
+				<div class="footer-nav__heading">Contact details</div>
+				<address class="address">
+				Head Office: Shivaji Nager.<br>
+				Sinner, Nashik.
+			</address>
+				<div class="phone">
+					Telephone:
+					<a class="phone__number" href="tel:9875785050">9875785050<br><tab></tab>9875875050</a>
+				</div>
+				<div class="email">
+					Email:
+					<a href="mailto:masterageoscience@gmail.com" class="email__addr">masteragroscience.com</a>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	
+
+	<div class="page-footer__subline">
+		<div class="container clearfix">
+
+			<div class="copyright">
+				&copy; Master Agro Science&trade;
+			</div>
+
+			
+			<div class="designby">
+			
+			</div>
+		</div>
+	</div>
+</footer>
