d04203ad91763983a541f949a72d50a0867acd0b
Rutujamore30 <116193578+Rutujamore30@users.noreply.github.com>
2023-11-24 22:07:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -113,7 +113,7 @@ include("includes/main.php");
           <div class="footer-nav__col footer-nav__col--contacts">
             <div class="footer-nav__heading">Contact details</div>
             <address class="address">
-            Head Office: Sajivaninagar.<br>
+            Head Office: Shivajinagar.<br>
             Sinner Nashik.
           </address>
             <div class="phone">
